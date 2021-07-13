bc75fef59507a3e503b429c9bf0bc32726ae32e8
Jacob Pfeifer <jakepf00@gmail.com>
2021-07-12 21:08:53 -0500

    Added CubeMath class

---
+++ CubeSolver/CubeSolver/CubeMath.cpp
@@ -0,0 +1,7 @@
+#include "CubeMath.h"
+
+CubeMath::Point::Point(int x, int y, int z) {
+	this->x = x;
+	this->y = y;
+	this->z = z;
+}
---
+++ CubeSolver/CubeSolver/CubeMath.h
@@ -0,0 +1,17 @@
+#pragma once
+class CubeMath
+{
+	class Point {
+	public:
+		Point(int x, int y, int z);
+	private:
+		int x;
+		int y;
+		int z;
+	};
+	class Matrix {
+	public:
+		Matrix();
+	};
+};
+
